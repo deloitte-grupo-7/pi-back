559a9d1845f39770d6008b06be3b3d4a22555668
luantzc <luantzc@gmail.com>
2021-10-20 11:20:16 -0300

    initial commit

---
+++ src/main/java/com/equipe7/getserv/GetservApplication.java
@@ -0,0 +1,13 @@
+package com.equipe7.getserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipe7/getserv/GetservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe7.getserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
