76608f9443c67a3fa43d44d30ed2c6942cb117e5
luantzc <luantzc@gmail.com>
2021-10-21 10:33:39 -0300

    Configurando entidade Usuario (e corrigindo identação do pom.xml)

--- src/main/java/com/equipe7/getserv/model/Usuario.java
+++ src/main/java/com/equipe7/getserv/model/Usuario.java
@@ -2,18 +2,106 @@ package com.equipe7.getserv.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
+	@NotNull
+	@Size(min = 1, max=128)
     private String nome;
+	
+	@NotNull
+	@Size(min = 11, max=11)
     private String cpf;
+
+	@NotNull
+	@Size(min = 8, max=65)
     private String email;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
     private Date nascimento;
+	
+	@NotNull
+	@Size(min = 3, max = 64)
     private String username;
+
+	@NotNull
+	@Size(min = 8, max = 128)
     private String senha;
 
     public Usuario(){
 
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
