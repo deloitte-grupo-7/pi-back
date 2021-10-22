34111886974c04f586a33afca15791b1d2b78f2c
luantzc <luantzc@gmail.com>
2021-10-21 21:18:06 -0300

    Iniciando as configuração do UsuarioController

--- src/main/java/com/equipe7/getserv/controller/UsuarioController.java
+++ src/main/java/com/equipe7/getserv/controller/UsuarioController.java
@@ -1,5 +1,33 @@
 package com.equipe7.getserv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.equipe7.getserv.model.Usuario;
+import com.equipe7.getserv.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping
+@CrossOrigin(value = "*", allowedHeaders = "*")
 public class UsuarioController {
+	//kutebrowser
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@PostMapping("/register")
+	public ResponseEntity<Usuario> post (@RequestBody Usuario user){
+		if (user.getCpf().matches("\\D")) {
+			//deve returnar uma erro de cpf inválido;
+		}
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(user));
+    }
+	
+	
 
 }
--- src/main/java/com/equipe7/getserv/repository/UsuarioRepository.java
+++ src/main/java/com/equipe7/getserv/repository/UsuarioRepository.java
@@ -1,4 +1,17 @@
 package com.equipe7.getserv.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
+
+import javax.swing.Spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.equipe7.getserv.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public List<Usuario> findAllbyNomeContainingIgnoreCase(Spring nome);
+	
 }
