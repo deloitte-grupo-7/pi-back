14b302b12cf2a0488f035aaef9324a31956eb95a
luantzc <luantzc@gmail.com>
2021-10-28 11:50:32 -0300

    Swagger Documentation adicionado

--- src/main/java/com/equipe7/getserv/security/BasicSecurityConfig.java
+++ src/main/java/com/equipe7/getserv/security/BasicSecurityConfig.java
@@ -1,4 +1,43 @@
 package com.equipe7.getserv.security;
 
-public class BasicSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable()
+//			.authorizeRequests()
+//			.anyRequest().authenticated()
+//			.and().httpBasic();
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.and().httpBasic()
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().csrf().disable();
+	}
+
+	@Autowired
+	public void configureGlobal (AuthenticationManagerBuilder authentication) throws Exception{
+		authentication.inMemoryAuthentication()
+			.withUser("admin")
+			.password(passwordEncoder().encode("equipe7"))
+			.authorities("ROLE_USER");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
