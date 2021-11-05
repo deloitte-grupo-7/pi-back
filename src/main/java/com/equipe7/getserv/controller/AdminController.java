84c9d603706e02c954cc32bb9a8c7c080ed75368
luantzc <luantzc@gmail.com>
2021-11-05 04:10:10 -0300

    Nova role pode ser adicionada a um usuário

--- src/main/java/com/equipe7/getserv/controller/AdminController.java
+++ src/main/java/com/equipe7/getserv/controller/AdminController.java
@@ -3,10 +3,10 @@ package com.equipe7.getserv.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.equipe7.getserv.controller.form.RoleToUserForm;
 import com.equipe7.getserv.repository.UserRepository;
 import com.equipe7.getserv.resource.Table;
 import com.equipe7.getserv.service.UserService;
@@ -26,9 +26,9 @@ public class AdminController {
 
 
 	@GetMapping("/addrole/{user}/{role}")
-	public ResponseEntity<RoleToUserForm> addRoleToUser(RoleToUserForm form){
-		userServ.addRoleToUser(form.getUsername(), form.getRoleName());
-		return ResponseEntity.ok(form);
+	public ResponseEntity<String> addRoleToUser(@PathVariable String user, @PathVariable String role){
+		userServ.addRoleToUser(user, role);
+		return ResponseEntity.ok(user + " : " + role);
 	}
 	
 	@GetMapping("/tables/reset")
